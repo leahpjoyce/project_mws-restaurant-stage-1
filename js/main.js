b824fcf6dc57b40754ae21c3f8ad57e64350acd7
Leah Joyce <leahpjoyce@gmail.com>
2018-08-14 21:40:34 -0500

    feat: Service Worker

--- js/main.js
+++ js/main.js
@@ -214,6 +214,14 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     } */
 
 
-navigator.serviceWorker.register('/sw.js')
-    .then(reg => console.log('SW registered!', reg))
-    .catch(err => console.log('Boo!', err));
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
