9b4bbec45ef57ade78cf4edc0a11be5e85b57131
Leah Joyce <leahpjoyce@gmail.com>
2018-08-18 00:19:10 -0500

    fix: Service Worker

--- sw_registration.js
+++ sw_registration.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", event => {
-    /**
-     *     Service Worker registration
-     */
-    if (navigator.serviceWorker) {
-        navigator.serviceWorker
-            .register("sw.js")
-            .then(registration => console.log("SW registered", registration))
-            .catch(e => console.log("Registration failed :(", e));
-    }
-});
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
